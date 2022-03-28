95a59bc99fe19beca242da4ababc45ffd399516e
Trevor Smith <trevorsm@pm.me>
2022-03-27 20:52:55 -0600

    removed build

--- server/build/api.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference path="../src/index.d.ts" />
-export declare function getTeacherRatings(id: string): Promise<any>;
-export declare function searchSchools(name: string): Promise<any>;
-export declare function searchTeachers(name: string, schoolID: string): Promise<any>;
